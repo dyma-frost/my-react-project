292f893035404535be32a73045a0061bc4a8a43e
dyma.frost <dyma.frost@gmail.com>
2023-03-12 23:30:31 +0200

    create FAQ

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -1,18 +1,54 @@
+import { useState } from 'react'
 import faqArray from 'utils/faqArray'
 import './FAQ.scss'
 
 type Props = {}
+
 const FAQ = (props: Props) => {
+    const [isActive, setActive] = useState(false)
+
+    const toggleClass = (id: number) => {
+        setActive((id) => !isActive)
+        console.log(id)
+        console.log(isActive)
+    }
+
     return (
         <>
             <div className="faq">
-                <div className="continer">
-                    {faqArray.map((item) => (
-                        <div className="title-wrapper" key={item.id}>
-                            <div className="question">{item.question}</div>
-                            <div className="answer">{item.answer}</div>
-                        </div>
-                    ))}
+                <div className="container">
+                    <h2>Frequently Asked Questions</h2>
+                    <div className="title-wrapper">
+                        {faqArray.map((item) => (
+                            <div className="ask-wrapper" key={item.id}>
+                                <div
+                                    className="question"
+                                    onClick={() => toggleClass(item.id)}
+                                >
+                                    <i
+                                        className="fa fa-plus-circle"
+                                        style={{ color: '#204289' }}
+                                    ></i>
+                                    <i
+                                        className={
+                                            isActive
+                                                ? 'fa fa-minus-circle show'
+                                                : 'fa'
+                                        }
+                                        style={{ color: '#204289' }}
+                                    ></i>
+                                    {item.question}
+                                </div>
+                                <div
+                                    className={
+                                        isActive ? 'show answer' : 'answer'
+                                    }
+                                >
+                                    {item.answer}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
--- src/utils/faqArray.ts
+++ src/utils/faqArray.ts
@@ -1,37 +1,37 @@
 export type Faq = {
-    id: string
+    id: number
     question: string
     answer: string
 }
 
 const faqArray: Faq[] = [
     {
-        id: 'q1',
+        id: 1,
         question: 'Nostra ultricies gravida luctus hac',
         answer: 'Suscipit dictum tellus senectus. Pede vehicula mattis lacinia ultrices diam luctus ornare tempor erat eu non. Ad leo aptent interdum libero neque si. Quis curabitur mus convallis fermentum at tempor per.',
     },
     {
-        id: 'q2',
+        id: 2,
         question: 'Malesuada velit dictum fusce vehicula',
         answer: 'Si enim amet sollicitudin eros. Penatibus suscipit elit ipsum accumsan morbi conubia non. Magna eu dolor semper risus himenaeos porta rhoncus venenatis primis. Hendrerit imperdiet venenatis in vitae per hac taciti ridiculus nullam at. Lobortis cras vestibulum metus risus montes conubia faucibus facilisi ligula. Gravida cursus ad sed ligula dis sit turpis curae. Elit vel platea per letius suscipit sapien ultricies rhoncus interdum.',
     },
     {
-        id: 'q3',
+        id: 3,
         question: 'Tellus in morbi accumsan letius',
         answer: 'quis vivamus iaculis lacus nascetur eget. Id quam dictumst cubilia cras turpis elit quis etiam felis aenean senectus.',
     },
     {
-        id: 'q4',
+        id: 4,
         question: 'Imperdiet diam nascetur si vehicula',
         answer: 'Pulvinar turpis magnis a in. Nostra massa duis praesent odio ornare vivamus aliquet.',
     },
     {
-        id: 'q5',
+        id: 5,
         question: 'Maximus mollis leo donec euismod',
         answer: 'Fermentum dapibus lorem enim ultrices natoque velit a. Eget velit rutrum convallis iaculis platea.',
     },
     {
-        id: 'q6',
+        id: 6,
         question: 'Eros fames curae ultrices',
         answer: 'Et facilisis elit bibendum efficitur litora tristique luctus. Potenti blandit rutrum donec laoreet curabitur. Phasellus dapibus nam consequat gravida libero dictumst cursus elit integer. Sapien interdum ipsum ultricies auctor tincidunt lobortis eget felis letius molestie lacus.',
     },
